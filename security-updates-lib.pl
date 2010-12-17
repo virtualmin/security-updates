08554ed05cd8a9e34feed33aa36b50f5e24246df
Jamie Cameron <jcameron@webmin.com>
2010-12-17 11:33:50 -0800

    Remove use of deprecated defined(@foo) https://sourceforge.net/tracker/?func=detail&atid=117457&aid=3138980&group_id=17457

--- security-updates-lib.pl
+++ security-updates-lib.pl
@@ -483,7 +483,7 @@ return defined(&software::update_system_updates) &&
 sub updates_available
 {
 my ($nocache, $all) = @_;
-if (!defined(@updates_available_cache)) {
+if (!length(@updates_available_cache)) {
 	if ($nocache || &cache_expired($updates_cache_file)) {
 		# Get from original source
 		@updates_available_cache = &software::update_system_updates();
