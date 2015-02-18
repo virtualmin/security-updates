915246839e1276f775009255c97390c69b6142fc
Jamie Cameron <jcameron@webmin.com>
2015-02-17 17:54:58 -0800

    Remove useless scalar call http://sourceforge.net/p/webadmin/bugs/4559

--- security-updates-lib.pl
+++ security-updates-lib.pl
@@ -483,7 +483,7 @@ return defined(&software::update_system_updates) &&
 sub updates_available
 {
 my ($nocache, $all) = @_;
-if (!scalar(@updates_available_cache)) {
+if (!@updates_available_cache) {
 	if ($nocache || &cache_expired($updates_cache_file)) {
 		# Get from original source
 		@updates_available_cache = &software::update_system_updates();
